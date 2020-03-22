c2b4bc74a7815cd67485d6f54ed6e78f414656ec
Fabio Pereira <fabiopereira.fps@gmail.com>
2020-03-22 03:36:53 -0300

    Remove getters and setters

--- src/main/java/br/edu/infnet/domain/Employee.java
+++ src/main/java/br/edu/infnet/domain/Employee.java
@@ -8,28 +8,4 @@ public abstract class Employee {
     protected String role;
 
     public abstract void calculateSalary();
-
-    public UUID getRegistration() {
-        return registration;
-    }
-
-    public void setRegistration(UUID registration) {
-        this.registration = registration;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
