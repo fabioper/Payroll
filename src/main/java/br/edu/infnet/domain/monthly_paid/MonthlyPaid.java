a2a172acc6180b8e034f65a402a8dd4f5bd064aa
Fabio Pereira <fabiopereira.fps@gmail.com>
2020-03-22 23:26:11 -0300

    Add ModelMapper and fix MonthlyPaid class name

--- src/main/java/br/edu/infnet/domain/monthly_paid/MonthlyPaid.java
+++ src/main/java/br/edu/infnet/domain/monthly_paid/MonthlyPaid.java
@@ -2,7 +2,7 @@ package br.edu.infnet.domain.monthly_paid;
 
 import br.edu.infnet.domain.Employee;
 
-public class HourlyPaid extends Employee {
+public class MonthlyPaid extends Employee {
     protected double salary;
 
     @Override
